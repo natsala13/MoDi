f36a01daab5f66b9e79a7d25f19c72fbd8685f86
nathansala <natouda@gmail.com>
2023-03-14 15:54:20 +0200

    test comparing both Fast Conv and 3d Conv to golden.

--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -1,11 +1,12 @@
-import pytest
+import time
 import torch
+import pytest
 import numpy as np
 import os.path as osp
 
 from utils.data import Edge
 from models.gan import Generator, Discriminator
-from utils.traits import SkeletonAwarePoolTraits, SkeletonAwareConv3DTraits, NonSkeletonAwareTraits
+from utils.traits import SkeletonAwareConv3DTraits, SkeletonAwareFastConvTraits
 
 N_MLP = 8
 LATENTS = 512
@@ -14,6 +15,7 @@ N_INPLACE_CONVOLUTIONS = 2
 
 DEVICE = 'cuda'
 FIXED_SEED = 1304
+EPSILON = 0.01
 CHECKPOINT_PATH = 'checkpoints/MoDi_u20_shab87_clearml_ffc4aa_079999.pt'
 DATA_PATH = 'tests/data/'
 GOLDEN_PATH = 'samples/golden_smaple_1304.npy'
@@ -22,25 +24,20 @@ GOLDEN_PATH = 'samples/golden_smaple_1304.npy'
 @pytest.fixture(autouse=True)
 def entity():
     Edge.enable_global_position()
-
     Edge.enable_foot_contact()
-    # args.axis_up = 1
 
     return Edge()
 
 
-@pytest.fixture(autouse=True)
-def generator(checkpoint, entity):
-    # if hasattr(args, 'n_frames_dataset') and args.n_frames_dataset == 128:
-    # SkeletonAwareConv3DTraits.set_num_frames(128)
-
-    generator = Generator(
-        LATENTS, N_MLP, traits_class=SkeletonAwareConv3DTraits, entity=entity, n_inplace_conv=N_INPLACE_CONVOLUTIONS
+@pytest.fixture
+def generator(checkpoint, entity, traits_class):
+    my_generator = Generator(
+        LATENTS, N_MLP, traits_class=traits_class, entity=entity, n_inplace_conv=N_INPLACE_CONVOLUTIONS
     ).to(DEVICE)
 
-    generator.load_state_dict(checkpoint["g_ema"])
+    my_generator.load_state_dict(checkpoint["g_ema"])
 
-    return generator
+    return my_generator
 
 
 @pytest.fixture(autouse=True)
@@ -66,11 +63,14 @@ def style(seed):
 
 @pytest.fixture
 def sample(generator, style):
+    before = time.time()
     motion, w_latent, _ = generator(
         [style], truncation=TRANCATION, truncation_latent=mean_latent,
         return_sub_motions=False, return_latents=True)
 
-    return motion
+    runtime = time.time() - before
+
+    return motion, runtime
 
 
 @pytest.mark.skip
@@ -78,8 +78,12 @@ def test_save_generate_golden(seed, sample):
     np.save(osp.join(DATA_PATH, f'motion_{seed}.npy'), sample[0].detach().cpu().numpy())
 
 
-def test_compare_golden_and_sampled(seed, sample):
+@pytest.mark.parametrize('traits_class', [SkeletonAwareConv3DTraits, SkeletonAwareFastConvTraits])
+def test_compare_golden_and_sampled(seed, sample, traits_class):
     golden = np.load('samples/golden_smaple_1304.npy')
+    motion, runtime = sample
 
-    assert (sample.detach().cpu().numpy() == golden).all()
+    assert torch.tensor(motion.detach().cpu().numpy() - golden).norm() < EPSILON
+
+    print(f'Runtime for class - {traits_class} is {runtime}')
 
