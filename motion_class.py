37f11a9312479ab98e2ab06d020d197e47781c77
nathansala <natouda@gmail.com>
2023-04-16 13:26:09 +0300

    moved tests to separate file.

--- motion_class.py
+++ motion_class.py
@@ -74,24 +74,12 @@ import torch
 import numpy as np
 from Motion import BVH
 
-import pytest
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from utils.data import Edge
-
-PATH = '/Users/nathansala/tau/code/MoDi/data/edge_rot_data.npy'
-BVH_EXAMPLE = 'tests/motion0.bvh'
-BVH_GENERATED = 'tests/generated_1304.bvh'
-BVH_SALUTE = 'tests/Salute.bvh'
 
 LEFT_FOOT_NAME = 'LeftFoot'
 RIGHT_FOOT_NAME = 'RightFoot'
-
-npy_data = np.load(PATH, allow_pickle=True)
-
-
-# animation, names, frametime = BVH.load(BVH_EXAMPLE)
 
 
 class EdgePoint(tuple):
@@ -426,82 +414,3 @@ class DynamicData:
     @property
     def static(self) -> StaticData:
         raise NotImplementedError
-
-
-@pytest.fixture(autouse=True)
-def static():
-    return StaticData.init_from_bvh(BVH_EXAMPLE)
-
-
-@pytest.fixture(autouse=True)
-def parents(static):
-    return static.parents_list[-1]
-
-
-@pytest.fixture(autouse=True)
-def degree(static, parents):
-    return static._topology_degree(parents)
-
-
-@pytest.fixture(autouse=True)
-def pooling(static, degree, parents):
-    return static._calculate_pooling_for_level(parents, degree)
-
-
-def test_parents(parents):
-    assert parents == [-1, 0, 1, 2, 3, 4, 5, 6, 4, 8, 9, 10, 11, 4,
-                                      13, 14, 15, 16, 0, 18, 19, 20, 0, 22, 23, 24]
-
-
-def test_degree(degree):
-    assert degree == [3, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]
-
-
-def test_sequences(static, degree, parents):
-    seq = static._find_seq(0, degree, parents)
-    assert seq == [[0, 1, 2, 3, 4], [4, 5, 6, 7], [4, 8, 9, 10, 11, 12], [4, 13, 14, 15, 16, 17], [0, 18, 19, 20, 21],
-                   [0, 22, 23, 24, 25]]
-
-
-def test_pooling_non_normalised(pooling):
-    assert pooling == {(0, 2): [(0, 1), (1, 2)], (2, 4): [(2, 3), (3, 4)], (4, 6): [(4, 5), (5, 6)], (6, 7): [(6, 7)],
-                       (4, 9): [(4, 8), (8, 9)], (9, 11): [(9, 10), (10, 11)], (11, 12): [(11, 12)],
-                       (4, 14): [(4, 13), (13, 14)],
-                       (14, 16): [(14, 15), (15, 16)], (16, 17): [(16, 17)], (0, 19): [(0, 18), (18, 19)],
-                       (19, 21): [(19, 20), (20, 21)],
-                       (0, 23): [(0, 22), (22, 23)], (23, 25): [(23, 24), (24, 25)]}
-
-
-def test_pooling_normalised(pooling):
-    normalised_pooling = StaticData._normalise_joints(pooling)
-    assert normalised_pooling == {(0, 1): [(0, 1), (1, 2)], (1, 2): [(2, 3), (3, 4)], (2, 3): [(4, 5), (5, 6)],
-                                  (3, 4): [(6, 7)],
-                                  (2, 5): [(4, 8), (8, 9)], (5, 6): [(9, 10), (10, 11)], (6, 7): [(11, 12)],
-                                  (2, 8): [(4, 13), (13, 14)],
-                                  (8, 9): [(14, 15), (15, 16)], (9, 10): [(16, 17)], (0, 11): [(0, 18), (18, 19)],
-                                  (11, 12): [(19, 20), (20, 21)],
-                                  (0, 13): [(0, 22), (22, 23)], (13, 14): [(23, 24), (24, 25)]}
-
-
-def test_all_parents(static):
-    assert static.parents_list[::-1][:-2] == [[-1, 0, 1, 2, 3, 4, 5, 6, 4, 8, 9, 10, 11, 4, 13, 14, 15, 16, 0, 18, 19, 20, 0, 22, 23, 24],
-                                        [-1, 0, 1, 2, 3, 2, 5, 6, 2, 8, 9, 0, 11, 0, 13], [-1, 0, 1, 1, 3, 1, 5, 0, 0],
-                                        [-1, 0, 1, 1, 1, 0, 0]]
-
-
-def test_all_pooling(static):
-    assert static.skeletal_pooling_dist_1_edges[::-1][:-2] == [
-        {(0, 1): [(0, 1), (1, 2)], (1, 2): [(2, 3), (3, 4)], (2, 3): [(4, 5), (5, 6)], (3, 4): [(6, 7)],
-         (2, 5): [(4, 8), (8, 9)], (5, 6): [(9, 10), (10, 11)], (6, 7): [(11, 12)], (2, 8): [(4, 13), (13, 14)],
-         (8, 9): [(14, 15), (15, 16)], (9, 10): [(16, 17)], (0, 11): [(0, 18), (18, 19)],
-         (11, 12): [(19, 20), (20, 21)], (0, 13): [(0, 22), (22, 23)], (13, 14): [(23, 24), (24, 25)]},
-        {(0, 1): [(0, 1), (1, 2)], (1, 2): [(2, 3), (3, 4)], (1, 3): [(2, 5), (5, 6)], (3, 4): [(6, 7)],
-         (1, 5): [(2, 8), (8, 9)], (5, 6): [(9, 10)], (0, 7): [(0, 11), (11, 12)], (0, 8): [(0, 13), (13, 14)]},
-        {(0, 1): [(0, 1)], (1, 2): [(1, 2)], (1, 3): [(1, 3), (3, 4)], (1, 4): [(1, 5), (5, 6)], (0, 5): [(0, 7)],
-         (0, 6): [(0, 8)]}]
-
-
-# plot_static = StaticData.init_from_bvh(BVH_EXAMPLE)
-# for parent in plot_static.parents_list:
-#     plot_static.plot(parent)
-#     plt.figure()
